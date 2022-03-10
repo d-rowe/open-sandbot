d465d6b5f43922e2a95b1f844ce09285a0487b45
Daniel Rowe <danielrowetech@gmail.com>
2022-03-10 14:33:57 -0800

    Merge testing branch to main

---
+++ server/bot/bot.py
@@ -0,0 +1,82 @@
+import atexit
+import math
+from adafruit_motor import stepper as STEPPER
+from adafruit_motorkit import MotorKit
+
+kit = MotorKit()
+__steps_per_degree: float = 4.77  # tweak for accuracy
+__arm_length: int = 190  # total arm length in mm
+__upper_stepper = kit.stepper1
+__lower_stepper = kit.stepper2
+__upper_position: int = 90 * __steps_per_degree
+__lower_position: int = 180 * __steps_per_degree
+
+
+def __step(stepper, direction):
+    if direction > 0:
+        stepper.onestep(direction=STEPPER.BACKWARD, style=STEPPER.INTERLEAVE)
+    if direction < 1:
+        stepper.onestep(direction=STEPPER.FORWARD, style=STEPPER.INTERLEAVE)
+
+
+def __upper_step(direction):
+    __step(__upper_stepper, -direction)
+
+
+def __lower_step(direction):
+    __step(__lower_stepper, direction)
+
+
+def to_arm_angles(angle1: float, angle2: float):
+    global __lower_position
+    global __upper_position
+
+    # convert angles to target step positions for steppers
+    target_lower_steps = round(angle1 * __steps_per_degree)
+    target_upper_steps = round(angle2 * __steps_per_degree)
+
+    # relative steps needed to get from current position to target
+    relative_lower_steps = target_lower_steps - __lower_position
+    relative_upper_steps = relative_lower_steps + target_upper_steps - __upper_position
+
+    tick = 0
+    target = max(abs(relative_lower_steps), 1) * max(abs(relative_upper_steps), 1)
+    while tick <= target:
+        upper_should_move = relative_lower_steps != 0 and (tick % relative_lower_steps) == 0
+        lower_should_move = relative_upper_steps != 0 and (tick % relative_upper_steps) == 0
+        if upper_should_move:
+            __upper_step(relative_lower_steps)
+        if lower_should_move:
+            __lower_step(relative_upper_steps)
+
+        tick += 1
+
+    __lower_position = target_lower_steps
+    __upper_position = target_upper_steps
+
+
+def to_theta_rho(theta: float, rho: float):
+    half_length = __arm_length / 2
+    rho_dist = __arm_length * rho
+    theta_degrees = math.degrees(theta)
+    upper_angle = 180 - math.degrees(
+        math.acos((((half_length ** 2) * 2) - (rho_dist ** 2)) / (2 * half_length * half_length)))
+    lower_angle_offset = upper_angle / 2
+    lower_angle = theta_degrees - lower_angle_offset
+
+    to_arm_angles(lower_angle, upper_angle)
+
+
+def to_home():
+    to_arm_angles(90, 180)
+
+
+def release():
+    __upper_stepper.release()
+    __lower_stepper.release()
+
+
+@atexit.register
+def exit():
+    to_home()
+    release()
--- server/index.py
+++ server/index.py
@@ -1,7 +1,9 @@
 import os
+from bot import bot
 from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_folder='../client/dist')
+
 
 # static assets
 @app.route('/', defaults={'path': ''})
@@ -12,19 +14,22 @@ def serve(path):
     else:
         return send_from_directory(app.static_folder, 'index.html')
 
-# api
+
 @app.route('/api/move', methods=['POST'])
 def movement():
+    bot.move_to_angles(90, 90)
     return '', 201
+
 
 @app.route('/api/software-update', methods=['POST'])
 def update():
     stream = os.popen('cd .. && ./update.sh')
     result = stream.read()
-    if ('compiled successfully' in result):
+    if 'compiled successfully' in result:
         return 'Updated successfully', 201
 
     return 'Failed to update', 500
 
+
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True, port=5000)
+    app.run(debug=True, use_reloader=True, port=5000, host="0.0.0.0")
