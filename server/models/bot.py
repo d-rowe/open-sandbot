082e7da841a4848d81701e73eb30ccd7dcfeda2c
Daniel Rowe <danielrowetech@gmail.com>
2022-05-31 08:36:58 -0700

    clean

--- server/models/bot.py
+++ server/models/bot.py
@@ -1,70 +1,67 @@
 import math
 from time import sleep
-
 from adafruit_motor import stepper as STEPPER
 from adafruit_motorkit import MotorKit
 
-kit = MotorKit()
-in_progress = False
-__steps_per_degree: float = 4.9  # tweak for accuracy
-__stepper_upper = kit.stepper1
-__stepper_lower = kit.stepper2
-__steps_upper: int = 0
-__steps_lower: int = 0
-__force_stop: bool = False
-__step_delay: int = 0
+# Hardware configuration
+_ARM_GEAR_TEETH = 60
+_STEPPER_GEAR_TEETH = 14
+_DEGREES_PER_STEP = 1.8
+
+_steps_per_degree: float = _ARM_GEAR_TEETH / _STEPPER_GEAR_TEETH / _DEGREES_PER_STEP * 2
+_kit = MotorKit()
+_stepper_upper = _kit.stepper1
+_stepper_lower = _kit.stepper2
+_steps_upper: int = 0
+_steps_lower: int = 0
+_step_delay: int = 0
+_force_stop: bool = False
+in_progress: bool = False
 
 
 def set_speed(speed: int):
-    global __step_delay
-    __step_delay = 100 - speed
+    global _step_delay
+    _step_delay = 100 - speed
 
 
 def stop():
-    global __force_stop
+    global _force_stop
     if in_progress:
-        __force_stop = True
+        _force_stop = True
 
 
 def set_home():
-    global __steps_upper
-    global __steps_lower
-    __steps_upper = 0
-    __steps_lower = 0
+    global _steps_upper
+    global _steps_lower
+    _steps_upper = 0
+    _steps_lower = 0
 
 
-def get_position_in_angles():
-    return {
-        "lower": __steps_lower / __steps_per_degree,
-        "upper": __steps_upper / __steps_per_degree
-    }
-
-
-def __step_once(stepper, direction):
+def _step_once(stepper, direction):
     if direction > 0:
         stepper.onestep(direction=STEPPER.BACKWARD, style=STEPPER.INTERLEAVE)
     if direction < 0:
         stepper.onestep(direction=STEPPER.FORWARD, style=STEPPER.INTERLEAVE)
 
 
-def __step_once_upper(direction):
-    global __steps_upper
-    __step_once(__stepper_upper, -direction)
-    __steps_upper += direction
+def _step_once_upper(direction):
+    global _steps_upper
+    _step_once(_stepper_upper, -direction)
+    _steps_upper += direction
 
 
-def __step_once_lower(direction):
-    global __steps_lower
-    __step_once(__stepper_lower, direction)
-    __steps_lower += direction
+def _step_once_lower(direction):
+    global _steps_lower
+    _step_once(_stepper_lower, direction)
+    _steps_lower += direction
 
 
 def to_arm_angles(angle1: float, angle2: float):
     global in_progress
-    global __steps_lower
-    global __steps_upper
-    global __force_stop
-    global __step_delay
+    global _steps_lower
+    global _steps_upper
+    global _force_stop
+    global _step_delay
 
     def get_direction(steps: int):
         if steps == 0:
@@ -75,27 +72,35 @@ def to_arm_angles(angle1: float, angle2: float):
         return
 
     # convert angles to target step positions for steppers
-    target_steps_lower = round(angle1 * __steps_per_degree)
-    target_steps_upper = round(angle2 * __steps_per_degree) + target_steps_lower
+    target_steps_lower = round(angle1 * _steps_per_degree)
+    target_steps_upper = round(angle2 * _steps_per_degree) + target_steps_lower
 
     # relative steps needed to get from current position to target
-    relative_steps_lower = target_steps_lower - __steps_lower
-    relative_steps_upper = target_steps_upper - __steps_upper
+    relative_steps_lower = target_steps_lower - _steps_lower
+    relative_steps_upper = target_steps_upper - _steps_upper
 
     if relative_steps_lower == 0 and relative_steps_upper == 0:
         # already at target position
         return
 
     is_lower_arm_faster = abs(relative_steps_lower) > abs(relative_steps_upper)
-    faster_steps = relative_steps_lower if is_lower_arm_faster else relative_steps_upper
-    slower_steps = relative_steps_upper if is_lower_arm_faster else relative_steps_lower
+
+    if is_lower_arm_faster:
+        faster_steps = relative_steps_lower
+        slower_steps = relative_steps_upper
+        faster_step_once = _step_once_lower
+        slower_step_once = _step_once_upper
+    else:
+        faster_steps = relative_steps_upper
+        slower_steps = relative_steps_lower
+        faster_step_once = _step_once_upper
+        slower_step_once = _step_once_lower
+
     faster_direction = get_direction(faster_steps)
     slower_direction = get_direction(slower_steps)
     abs_faster_steps = abs(faster_steps)
     abs_slower_steps = abs(slower_steps)
     speed_ratio = abs_slower_steps / abs_faster_steps
-    faster_step_once = __step_once_lower if is_lower_arm_faster else __step_once_upper
-    slower_step_once = __step_once_upper if is_lower_arm_faster else __step_once_lower
 
     in_progress = True
 
@@ -103,8 +108,8 @@ def to_arm_angles(angle1: float, angle2: float):
     slower_creep = 0
     for i in range(abs_faster_steps):
         # handle stop command
-        if __force_stop:
-            __force_stop = False
+        if _force_stop:
+            _force_stop = False
             break
 
         faster_step_once(faster_direction)
@@ -113,8 +118,8 @@ def to_arm_angles(angle1: float, angle2: float):
             slower_step_once(slower_direction)
             slower_creep -= 1
 
-        if __step_delay > 0:
-            sleep(__step_delay / 1000)
+        if _step_delay > 0:
+            sleep(_step_delay / 1000)
 
     in_progress = False
 
@@ -135,5 +140,5 @@ def to_home():
 
 def release():
     stop()
-    __stepper_upper.release()
-    __stepper_lower.release()
+    _stepper_upper.release()
+    _stepper_lower.release()
